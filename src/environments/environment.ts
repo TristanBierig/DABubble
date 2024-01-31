51e7dae79ba9f25231b19ccd882cd8dcd62030a6
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2024-01-31 21:19:49 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,12 +8,3 @@ export const environment = {
     appId: "1:680227045274:web:83dd5253e8a6a45ee3c423"
   },
 };
-
-// firebase: {
-//   projectId: 'dabubble-185ec',
-//   appId: '1:987069744589:web:39e82f06ef79331f03b084',
-//   storageBucket: 'dabubble-185ec.appspot.com',
-//   apiKey: 'AIzaSyDsHJf2mdjFcxVSSer0kRDd8imu523_VGs',
-//   authDomain: 'dabubble-185ec.firebaseapp.com',
-//   messagingSenderId: '987069744589',
-// },
